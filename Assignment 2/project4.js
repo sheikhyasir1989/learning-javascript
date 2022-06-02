b0bbf760f96f6d559cc41db42c2881125c0d5a9a
sheikhyasir1989 <sheikhyasir1989@gmail.com>
2022-06-02 15:21:51 +0500

    All assignment are been uploded on git

---
+++ Assignment 2/project4.js
@@ -0,0 +1,24 @@
+var abc = document.querySelector("ul");
+abc.style.backgroundColor = "lightgreen";
+abc.style.listStyle = "none";
+abc.style.display = "flex";
+
+
+
+
+
+var abc2 = document.querySelectorAll("li");
+
+for (var i = 0; i <= abc2.length; i++) {
+
+    abc2[i].style.padding = "23px";
+    abc2[i].style.cursor = "pointer";
+
+
+}
+
+
+abc.style.margin = "0";
+
+var body = document.querySelector("body");
+body.style.margin = "0";
